2a9f340b7461e38ba38d83d6747b3c5473363681
erikascicenas <erikas.cicenas@ftmc.lt>
2023-12-15 03:57:40 +0200

    Add full MPI functionality

--- primes-seq.cpp
+++ primes-seq.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <fstream>
 #include <cmath>
 #include <cstdlib>
 #include <cstdint>
@@ -12,6 +13,7 @@
 
 using cint = std::int64_t;
 
+constexpr double MINIMUM_TIME_UPDATE = 5; //seconds
 
 // const cint SEGMENT = 20;
 
@@ -27,21 +29,22 @@ int main(int argc, char** argv) {
 
     //Currently, basic segmented sieve.
     if(argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " MAXNUM MAX_MEMORY(GB) JOBS\n";
+        std::cerr << "Usage: " << argv[0] << " MAXNUM SEGMENT_LENGTH SAVE_PRIMES \n"; //TODO fix
         return -1;
     }
 
     cint maxnum = std::atoll(argv[1]);
     maxnum = maxnum % 2 ? maxnum : maxnum + 1;
-    std::cerr << nproc*message_length(maxnum)*sizeof(cint) << std::endl;
-    const cint MAX_MEMORY = std::atof(argv[2])*1000000000 - nproc*message_length(maxnum)*sizeof(cint); //1GB
-    assert(MAX_MEMORY > 0);
-    const cint MAX_SEGMENT = MAX_MEMORY / (2*sizeof(bool)*nproc);
-    assert(MAX_SEGMENT > 1000);
-    const cint SEGMENT = MAX_SEGMENT / std::atoi(argv[3]);
+    const cint SEGMENT = std::atoll(argv[2]);
+    const bool SAVEPRIMES = argv[3][0] == 't';
+
+    if(!pid)
+        std::cout << "ARGUMENTS:\n" << "MAXNUM: " << maxnum << "\nSEGMENT LENGTH: " << SEGMENT << 
+            "\nSAVE? " << (SAVEPRIMES ? "true" : "false") << std::endl;
 
     cint iters = std::ceil(static_cast<double>(maxnum - 1)/(2*SEGMENT + 2)); //todo: load splitting
     std::vector<cint> primes;
+    cint prime_num = 0;
     //First, find primes in sqrt(N) interval for other processes to use
     cint maxnum_init = std::sqrt(maxnum)+1;
     cint init_iters = std::ceil(static_cast<double>(maxnum_init - 1)/(2*SEGMENT + 2));
@@ -50,8 +53,8 @@ int main(int argc, char** argv) {
     double t_start = MPI_Wtime();
 
     if(!pid) {
+        std::cout << "Initializing sieving primes\n";
         for(cint i = 0; i < init_iters; ++i) {
-            std::cout << "Initializing sieving prines\n";
             cint start = 3 + 2*i + 2*i*SEGMENT;
             cint end = 3 + 2*i + 2*(i+1)*SEGMENT;
             end = end > maxnum ? maxnum : end;
@@ -67,38 +70,64 @@ int main(int argc, char** argv) {
 
     primes.reserve(message_length(maxnum));
 
-    if(!pid) std::cout << std::fixed << std::setprecision(2) << MPI_Wtime() << ":\tStarting main sieving!\n";
+    if(!pid) std::cout << std::fixed << std::setprecision(2) << MPI_Wtime() << "s:\tStarting main sieving!\n";
+
+    double last_update = MPI_Wtime();
+    cint last_i = pid;
 
     for(cint i = init_iters + pid; i < iters; i += nproc) {
-        std::cout << std::fixed;
-        std::cout << std::setprecision(1) << MPI_Wtime() << "s: [" << pid << "] " << 100.0*(i-pid)/(iters - init_iters) << "%\t";
-        std::cout << "Current number of primes found by process: " << primes.size() << std::endl;
+        if(MPI_Wtime() - last_update >= MINIMUM_TIME_UPDATE) {
+            double time_intv = MPI_Wtime() - last_update;
+            last_update += time_intv;
+            std::cout << std::fixed;
+            std::cout << std::setprecision(1) << last_update << "s: [" << pid << "] " << 100.0*(i-pid)/(iters - init_iters) << "%\t";
+            std::cout << "Current number of primes found by process: " << (SAVEPRIMES ? primes.size() : prime_num);
+            std::cout << "\tETA: " << time_intv/(i - last_i)*(iters - init_iters - i + pid) << 's' << std::endl;
+            last_i = i;
+        }
         cint start = 3 + 2*i + 2*i*SEGMENT;
         cint end = 3 + 2*i + 2*(i+1)*SEGMENT;
         end = end > maxnum ? maxnum : end;
         // std::cout << i << std::endl; TODO add pretty output
         Sieve s(end, start, i_primes); //Lifetime until loop end
-        primes.insert(std::end(primes), std::begin(s.getprimevector()), std::end(s.getprimevector()));
+        if(SAVEPRIMES)
+            primes.insert(std::end(primes), std::begin(s.getprimevector()), std::end(s.getprimevector()));
+        else
+            prime_num += s.getnumprimes();
     }
 
     if(!pid) std::cout << MPI_Wtime() << "s:\tPrime sieving complete; collecting results\n";
+    
+    cint msg_length, *recvbuf;
 
-    cint msg_length = message_length(maxnum);
-    assert(primes.size() < msg_length);
-    primes.resize(msg_length, -1);
-    cint* recvbuf = new cint[nproc*msg_length];
-    MPI_Gather(primes.data(), primes.size(), MPI_INT64_T, recvbuf, msg_length, MPI_INT64_T, 0, MPI_COMM_WORLD);
+    if(SAVEPRIMES) {
+        msg_length = message_length(maxnum);
+        primes.resize(msg_length, -1);
+        recvbuf = new cint[nproc*msg_length];
+        MPI_Gather(primes.data(), primes.size(), MPI_INT64_T, recvbuf, msg_length, MPI_INT64_T, 0, MPI_COMM_WORLD);
+    } else {
+        recvbuf = new cint[nproc];
+        MPI_Gather(&prime_num, 1, MPI_INT64_T, recvbuf, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
+    }
 
     if(!pid) {
-        i_primes.reserve(nproc*msg_length);
-        for(cint i = 0; i < nproc*msg_length; ++i) {
-            if(recvbuf[i] <= 0) continue;
-            i_primes.push_back(recvbuf[i]);
-        }
-        i_primes.insert(i_primes.begin(), 2);
-        // for(auto p : i_primes) std::cout << p << std::endl;
-        std::cout << "Found primes: " << i_primes.size() << std::endl;
         std::cout << "Time: " << MPI_Wtime() - t_start << " seconds\n";
+        if(SAVEPRIMES) {
+            i_primes.reserve(nproc*msg_length);
+            for(cint i = 0; i < nproc*msg_length; ++i) {
+                if(recvbuf[i] <= 0) continue;
+                i_primes.push_back(recvbuf[i]);
+            }
+            i_primes.insert(i_primes.begin(), 2);
+            std::cout << "Found primes: " << i_primes.size() << std::endl;
+            std::ofstream prime_out("primes.out");
+            for(auto p : i_primes) prime_out << p << std::endl;
+        } else {
+            prime_num = i_primes.size() + 1; //+1 for 2
+            for(int i = 0; i < nproc; ++i)
+                prime_num += recvbuf[i];
+            std::cout << "Found primes: " << prime_num << std::endl;
+        }
     }
 
     delete[] recvbuf;
